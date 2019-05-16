0f5db8e4b3e34a3004139452932b3aed94d9fb66
Yasachandra Bansal <yasachandra@gmail.com>
2019-05-16 21:04:10 +0530

    Implemented pagination for the search results

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -30,16 +30,48 @@ const limitRecipeTitle = (title,limit=17) => {
     return title;
 }
 
+const createButton = (page,type) => `
+    <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+        <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+        </svg>
+    </button>
+`;
+
+const renderButtons = (page, numResults, resPerPage) => {
+    const pages = Math.ceil(numResults/resPerPage);
+    let button;
+    if(page === 1 && pages > 1) {
+        // Show only button to go to next page
+        button = createButton(page,'next');
+    } else if(page > 1 && page < pages) {
+        // Show buttons to go to next and previous page respectively
+        button = `${createButton(page,'prev')}${createButton(page,'next')}`;
+    } else if(page === pages && pages > 1) {
+        // Show only button to go to previous page
+        button = createButton(page,'prev');
+    }
+    elements.searchResPages.insertAdjacentHTML('afterbegin',button);
+}
+
 export const getInput = () => elements.searchInput.value;
 
 export const clearInput = () => {
     elements.searchInput.value = '';
 }
 
-export const renderResults = recipes => {
-    recipes.forEach(renderRecipe);
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+    // Display the results for the current page
+    const start = (page - 1) * resPerPage;
+    const end = page * resPerPage;
+    recipes.slice(start,end).forEach(renderRecipe);
+
+    // Render the buttons to go to next or previous page
+    renderButtons(page,recipes.length,resPerPage);
 }
 
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
+    elements.searchResPages.innerHTML = '';
 }
